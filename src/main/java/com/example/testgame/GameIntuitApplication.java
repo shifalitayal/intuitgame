bded506f96eadade2565a44ebcb05c71de504ebb
shifalitayal <tayalshifali@gmail.com>
2024-05-13 11:53:58 -0700

    This is the master branch with db setup only

---
+++ src/main/java/com/example/testgame/GameIntuitApplication.java
@@ -0,0 +1,15 @@
+package com.example.testgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameIntuitApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(GameIntuitApplication.class, args);
+    }
+
+
+}
